76255d6049e10d1591935857bbb1d76f3bebb87c
Thejus <theju45official@gmail.com>
2026-06-29 12:43:51 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   'https://hgxyysanxbnjbehkrvhy.supabase.co',
-  'YOUR_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhneHl5c2FueGJuamJlaGtydmh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTg5MzQsImV4cCI6MjA5ODIzNDkzNH0.HsKrkvn4duQphw52FYptCf1oBKw-A62f6y2wHtOvNCg'
 )
